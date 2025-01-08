00071dad3c688e4566987711ecfe4acd04bd0db8
JagritiRastogi <jagritirastogi94@gmail.com>
2025-01-08 17:15:11 +0530

    Add JS file

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
+
+function toggleMenu() {
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+icon.addEventListener("click", toggleMenu);
